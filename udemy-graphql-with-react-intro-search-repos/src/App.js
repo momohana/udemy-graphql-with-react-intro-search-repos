71f217e12cc2ca69b3d19748511de28ad550615e
momohana <ttnarita@gmail.com>
2021-11-29 13:09:44 +0900

    setup dotenv

--- udemy-graphql-with-react-intro-search-repos/src/App.js
+++ udemy-graphql-with-react-intro-search-repos/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+console.log({GITHUB_TOKEN})
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello, GraphQL</div>
+    )
+  }
 }
 
-export default App;
+export default App
